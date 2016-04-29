36b38f7e7ac7a30415be91ca68f3eedd702de809
Angel <lopez_angel@hotmail.com>
2016-04-29 16:14:16 -0400

    fix some bugs

--- ExamStack/MainExams.cs
+++ ExamStack/MainExams.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using Telerik.WinControls.UI;
 
@@ -104,7 +100,7 @@ namespace ExamStack
             {
                 outForChecking.RemoveAt(pos);
                 MessageBox.Show("Exam has been returned");
-)
+
             }
         }
     }
